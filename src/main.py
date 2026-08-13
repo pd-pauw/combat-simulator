ed2d4316858738626ab397a2064ccedb97c46ccf
pd-pauw <pieter_depauw@hotmail.com>
2026-08-13 14:13:55 +0200

    Testing windows

--- src/main.py
+++ src/main.py
@@ -6,13 +6,16 @@ def main(stdscr):
     max_y,max_x  = stdscr.getmaxyx()
     y = max_y // 2
     x = max_x // 2
-
+    window_left = curses.newwin(max_y,30, 0, 0)
     stdscr.refresh()
-    window = curses.newwin(15,15, y, x)
+    window_middle = curses.newwin(max_y,100, 0, 32)
     stdscr.refresh()
-    window.box()
-    window.addstr(1,2,"Main Menu")
-    window.refresh()
+    window_left.box()
+    window_middle.box()
+    window_left.addstr(4,2,f"{max_y} , {max_x}")
+    window_left.addstr(1,2,"this is the left window")
+    window_middle.refresh()
+    window_left.refresh()
     stdscr.getch()
 
 
