ca138e15e3218dfd3116880cd9d8dc8b29c4d84a
pd-pauw <pieter_depauw@hotmail.com>
2026-08-14 15:46:51 +0200

    some small changes

--- src/Character.py
+++ src/Character.py
@@ -1,4 +1,28 @@
+from enum import Enum
+
+class Action_orientation(Enum):
+    LEFT = 1
+    UP = 2
+    RIGHT = 3
+    DOWN = 4
+
 class Character:
     def __init__(self, health, armor):
         self.health = health
         self.armor = armor
+        self.attack = 0
+        self.defence = 0
+
+        def defend(action_orientation: Action_orientation):
+            pass
+        def attack(action_orientation: Action_orientation):
+            pass
+        def use_consumable():
+            pass
+
+class PlayerCharacter(Character):
+    pass
+
+class EnemyCharacter(Character):
+    pass
+
