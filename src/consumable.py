5fe75801b9e1e9dc61d749c51f05606986a5c9e1
pd-pauw <pieter_depauw@hotmail.com>
2026-08-19 20:04:42 +0200

    Added healing potion

--- src/consumable.py
+++ src/consumable.py
@@ -1,5 +1,15 @@
+from character import Character
 class Consumable:
     def __init__(self, name):
         self.name = name
+
     def consume():
         pass
+
+class HealingPotion(Consumable):
+    def __init__(self, name, heal_value):
+        self.heal_value = heal_value 
+        super().__init__(name)
+
+    def consume(self, character: Character):
+        character.health += self.heal_value
